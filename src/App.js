be69eb22162e28377bc69a2f940187891f706f5f
Jenell Pizarro <nellarro@gmail.com>
2016-08-09 08:16:23 -0400

    just the beginning

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <header>hodor.</header>
+          <nav>
+          <ul>
+            <li>hodor...  </li>
+            <li>  hodor</li>
+            <li> Hodor</li>
+          </ul>
+          </nav>
+        </div>
+        <p className='App-article'>
+          <p>Hodor hodor hodor; hodor hodor hodor! Hodor, hodor, hodor. Hodor hodor hodor HODOR hodor, hodor hodor, hodor. Hodor hodor. Hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor hodor, hodor. Hodor hodor hodor hodor. Hodor. Hodor. Hodor hodor, hodor. Hodor hodor; hodor hodor. Hodor. Hodor hodor, hodor. Hodor HODOR hodor, hodor hodor?! </p>
+        </p>
+        <img className ="App-image"></img>
+
+      </div>
+
+    )
+  }
+}
+
+export default App
